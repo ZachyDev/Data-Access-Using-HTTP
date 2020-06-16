4d7fd0017b2b771dde71675a14719512ae14c32f
ZachyDev <mosetizachary001@gmail.com>
2020-06-16 11:24:56 +0300

    Making HTTP requests using jquery

---
+++ HTTP-using-jquery/main.js
@@ -0,0 +1,29 @@
+import axios from 'jquery';
+let promise = $.get('https://5ee87855ffee0c0016a12ddd.mockapi.io/api/v1/users');
+promise
+    .then(data => {
+    console.log(data);
+})
+    .catch(error => {
+        console.log(error);
+    });
+
+    // posting data using jquery
+
+// define user object
+
+const user = {
+    name: "Zachary",
+    avatar: "zachy.jpg"
+}
+
+let postData = axios.post(
+    'https://5ee87855ffee0c0016a12ddd.mockapi.io/api/v1/users',user
+);
+promise
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
